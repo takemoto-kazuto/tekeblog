77930f8bff8ea86f00610b84b58d0caa277f7184
takemotokazuto <takemoto.kazuto19@gmail.com>
2020-05-20 00:28:18 +0900

    タイトル表示作業途中

--- takeblogs/models.py
+++ takeblogs/models.py
@@ -8,4 +8,10 @@ class Post(models.Model):
 # データベースをあたかも変数かのように扱えるようにする
 # 役割をもつclassをmodels.pyに定義
 
+def __str__(self):
+    return self.title
+# defとは、英語のdefine（定義する）の略
+# str()は、数値を文字列に変換する機能で、strは Strings（文字列）の意味
+# self.titleで自身のtitleを返す
+
 # Create your models here.
--- takeblogs/views.py
+++ takeblogs/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Post # admin.pyにも入っている
+# Postクラスの変数をviewで利用する宣言
 
 def index(request):
-    return render(request, 'takeblogs/index.html')
+    posts = Post.objects.order_by('-published')
+    return render(request, 'takeblogs/index.html',{'posts': posts})
+    # order_by＝並び替え　published＝公開・代入
+    # htmlに変数を渡すために、render()内に{'posts': posts}と追記
+    # レンダリングとは「ある情報を形を変えて表現すること」
+    # renderメソッドは、request、template_name、contextという3つの引数を取る
 
 # Create your views here.
